b02cd7727470eb8e57e41551a3b52d1c21b2f5f8
nastya <nastya.zavalkina@gmil.com>
2013-11-13 18:16:33 +0100

    18: small style changes, changed address on the map

--- static/js/map.js
+++ static/js/map.js
@@ -1,7 +1,12 @@
 //http://code.google.com/p/jquery-ui-map/
+$('#map_canvas').gmap('closeInfoWindow');
 $(function() {
               $('#map_canvas').gmap().bind('init', function(event, map) {
-                  $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '48.0001730, 37.8571290', 'bounds': true });
-                      var zoom = $('#map_canvas').gmap('option', 'zoom', 17);
+                  $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '48.018951,37.859035', 'bounds': true }).click(function() {
+                $('#map_canvas').gmap('openInfoWindow',
+                    { 'content':  '<a href = "http://goo.gl/maps/3GgzU">Магазин "Умейка"</a> ' +
+                        '<p>ул. Кадиеская 6, Донецк</p>'}, this);
+                  });
+                  var zoom = $('#map_canvas').gmap('option', 'zoom', 17);
         });
 });
