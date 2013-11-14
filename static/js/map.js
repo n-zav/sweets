508bb64300e32a118a4dcb8cd95c0dba616fc4c9
nastya <nastya.zavalkina@gmil.com>
2013-11-14 16:31:11 +0100

    21: map infowindow fixed

--- static/js/map.js
+++ static/js/map.js
@@ -3,10 +3,10 @@ $('#map_canvas').gmap('closeInfoWindow');
 $(function() {
               $('#map_canvas').gmap().bind('init', function(event, map) {
                   $('#map_canvas').gmap('addMarker', { /*id:'m_1',*/ 'position': '48.018951,37.859035', 'bounds': true }).click(function() {
-                $('#map_canvas').gmap('openInfoWindow',
+                  $('#map_canvas').gmap('openInfoWindow',
                     { 'content':  '<a href = "http://goo.gl/maps/3GgzU">Магазин "Умейка"</a> ' +
-                        '<p>ул. Кадиеская 6, Донецк</p>'}, this);
+                        '<p>ул. Кадиеская 6, Донецк</p>', 'maxWidth': '300'}, this);
                   });
                   var zoom = $('#map_canvas').gmap('option', 'zoom', 17);
-        });
-});
+                });
+              });
